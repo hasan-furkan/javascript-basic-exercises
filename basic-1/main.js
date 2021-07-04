b7d0b45d4d9073701dfed7326b6082699336320d
hasan-furkan <hsnfrkn32@gmail.com>
2021-07-04 23:40:55 +0300

    basic-exercise-1

---
+++ basic-1/main.js
@@ -0,0 +1,89 @@
+        // var name = prompt("enter name");
+        // var no = prompt("enter student number");
+        // var lessonName = prompt("enter lesson name");
+        // var firstLessonNote = prompt("Enter a first note");
+        // var secondLessonNote = prompt("Enter a second note");
+        // var calculateAvarage = (Number(firstLessonNote) + Number(secondLessonNote)) / 2;
+        // console.log(no + " numarali " + name + " isimli " + "ogrencinin matematik sinavindan aldigi not ortalamasi " + calculateAvarage);
+
+        // var driver = prompt("enter a burn year")
+        // var yil = 2021
+        // var age = yil - driver
+        // if (age >= 18) {
+        //     console.log("Ehliyet alabilirsiniz");
+        // } else if (age < 18) {
+        //     console.log("ehliyet alamazsiniz ehliyet alabilmek icin " + (18 - age) + " sene daha beklemeniz gerek");
+        // } else {
+        //     console.log("yanlis bir deger girdiniz");
+        // }
+
+
+        // var text;
+        // var tarih = 7
+        // switch (tarih) {
+        //     case 1:
+        //         text = "ocak";
+        //         break;
+        //     case 2:
+        //         text = "subat";
+        //         break;
+        //     case 3:
+        //         text = "mart";
+        //         break;
+        //     case 4:
+        //         text = "nisan";
+        //         break;
+        //     case 5:
+        //         text = "mayis";
+        //         break;
+        //     case 6:
+        //         text = "haziran";
+        //         break;
+        //     case 7:
+        //         text = "temmuz";
+        //         break;
+        //     case 8:
+        //         text = "agustos";
+        //         break;
+        //     case 9:
+        //         text = "eylul";
+        //         break;
+        //     case 10:
+        //         text = "ekim";
+        //         break;
+        //     case 11:
+        //         text = "kasim";
+        //         break;
+        //     case 12:
+        //         text = "aralik";
+        //         break;
+        // }
+        // console.log(tarih, text);
+
+
+
+
+        // for (var i = 10; i < 101; i++) {
+        //     console.log(i);
+        // }
+
+
+        // var i = 10;
+        // while (i < 101) {
+        //     console.log(i);
+        //     i += 5
+        // }
+
+        // var name = "guest"
+        // var pw = "12345"
+        // var loggin = false
+        // while (loggin == false) {
+        //     var username = prompt("enter a username");
+        //     var password = prompt("enter a password");
+        //     if ((name == username) && (pw == password)) {
+        //         console.log("succesfull login");
+        //         loggin = true;
+        //     } else {
+        //         console.log("try");
+        //     }
+        // }
