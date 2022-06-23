02f85915655dc352091684293a4ccbb9225b8e18
hasan-furkan <81443587+hasan-furkan@users.noreply.github.com>
2022-06-23 15:06:31 +0300

    arrays

---
+++ arrays.js
@@ -0,0 +1,174 @@
+let numbers = [0, 3.14, 9.81, 37, 98.6, 100] // array of numbers
+let fruits = ['banana', 'orange', 'mango', 'lemon'] // array of strings, fruits
+let vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot'] // array of strings, vegetables
+let animalProducts = ['milk', 'meat', 'butter', 'yoghurt'] // array of strings, products
+let webTechs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB'] // array of web technologies
+let countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland'] // array of strings, countries
+
+// Print the array and its length
+
+console.log('Numbers:', numbers)
+console.log('Number of numbers:', numbers.length)
+
+console.log('Fruits:', fruits)
+console.log('Number of fruits:', fruits.length)
+
+console.log('Vegetables:', vegetables)
+console.log('Number of vegetables:', vegetables.length)
+
+console.log('Animal products:', animalProducts)
+console.log('Number of animal products:', animalProducts.length)
+
+console.log('Web technologies:', webTechs)
+console.log('Number of web technologies:', webTechs.length)
+
+console.log('Countries:', countries)
+console.log('Number of countries:', countries.length)
+
+let js = 'JavaScript'
+const charsInJavaScript = js.split('')
+
+console.log(charsInJavaScript) // ["J", "a", "v", "a", "S", "c", "r", "i", "p", "t"]
+
+let companiesString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+const companies = companiesString.split(',')
+
+console.log(companies) // ["Facebook", " Google", " Microsoft", " Apple", " IBM", " Oracle", " Amazon"]
+let txt =
+  'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+let words = txt.split(' ')
+
+console.log(words)
+// the text has special characters think how you can just get only the words
+// ["I", "love", "teaching", "and", "empowering", "people.", "I", "teach", "HTML,", "CSS,", "JS,", "React,", "Python"]
+
+numbers = [1, 2, 3, 4, 5]
+numbers[0] = 10      // changing 1 at index 0 to 10
+numbers[1] = 20      // changing  2 at index 1 to 20
+
+const eightEmptyValues = Array(8) // it creates eight empty values
+console.log(eightEmptyValues) // [empty x 8]
+
+const eightXvalues = Array(8).fill('X') // it creates eight element values filled with 'X'
+console.log(eightXvalues) // ['X', 'X','X','X','X','X','X','X']
+
+const eight0values = Array(8).fill(0) // it creates eight element values filled with '0'
+console.log(eight0values) // [0, 0, 0, 0, 0, 0, 0, 0]
+
+const four4values = Array(4).fill(4) // it creates 4 element values filled with '4'
+console.log(four4values) // [4, 4, 4, 4]
+
+const firstList = [1, 2, 3]
+const secondList = [4, 5, 6]
+const thirdList = firstList.concat(secondList)
+
+console.log(thirdList) // [1, 2, 3, 4, 5, 6]
+
+numbers = [1, 2, 3, 4, 5]
+
+console.log(numbers.indexOf(5)) // -> 4
+console.log(numbers.indexOf(0)) // -> -1
+console.log(numbers.indexOf(1)) // -> 0
+console.log(numbers.indexOf(6)) // -> -1
+
+
+numbers = [1, 2, 3, 4, 5, 3, 1, 2]
+
+console.log(numbers.lastIndexOf(2)) // 7
+console.log(numbers.lastIndexOf(0)) // -1
+console.log(numbers.lastIndexOf(1)) //  6
+console.log(numbers.lastIndexOf(4)) //  3
+console.log(numbers.lastIndexOf(6)) // -1
+
+
+webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ] // List of web technologies
+  
+  console.log(webTechs.includes('Node'))  // true
+  console.log(webTechs.includes('C'))     // false
+
+let numbers = [1, 2, 3, 4, 5]
+console.log(Array.isArray(numbers)) // true
+
+let number = 100
+console.log(Array.isArray(number)) // false
+
+let numbers = [1, 2, 3, 4, 5]
+console.log(numbers.toString()) // 1,2,3,4,5
+
+let names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+console.log(names.toString()) // Asabeneh,Mathias,Elias,Brook
+
+names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+
+console.log(names.join()) // Asabeneh,Mathias,Elias,Brook
+console.log(names.join('')) //AsabenehMathiasEliasBrook
+console.log(names.join(' ')) //Asabeneh Mathias Elias Brook
+console.log(names.join(', ')) //Asabeneh, Mathias, Elias, Brook
+console.log(names.join(' # ')) //Asabeneh # Mathias # Elias # Brook
+
+numbers = [1,2,3,4,5]
+
+  console.log(numbers.slice()) // -> it copies all  item
+  console.log(numbers.slice(0)) // -> it copies all  item
+  console.log(numbers.slice(0, numbers.length)) // it copies all  item
+  console.log(numbers.slice(1,4)) // -> [2,3,4] // it doesn't include the ending position
+
+
+  numbers = [1, 2, 3, 4, 5, 6]
+	numbers.splice(3, 3, 7, 8, 9)
+  console.log(numbers.splice(3, 3, 7, 8, 9))  // -> [1, 2, 3, 7, 8, 9] //it removes three item and replace three items
+
+ fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.push('apple')
+console.log(fruits)    // ['banana', 'orange', 'mango', 'lemon', 'apple']
+
+fruits.push('lime')
+console.log(fruits)   // ['banana', 'orange', 'mango', 'lemon', 'apple', 'lime']
+
+numbers = [1, 2, 3, 4, 5]
+numbers.pop() // -> remove one item from the end
+console.log(numbers) // -> [1,2,3,4]
+
+numbers = [1, 2, 3, 4, 5]
+numbers.shift() // -> remove one item from the beginning
+console.log(numbers) // -> [2,3,4,5]
+
+numbers = [1, 2, 3, 4, 5]
+numbers.unshift(0) // -> add one item from the beginning
+console.log(numbers) // -> [0,1,2,3,4,5]
+
+numbers = [1, 2, 3, 4, 5]
+numbers.reverse() // -> reverse array order
+console.log(numbers) // [5, 4, 3, 2, 1]
+
+webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ]
+  
+  webTechs.sort()
+  console.log(webTechs) // ["CSS", "HTML", "JavaScript", "MongoDB", "Node", "React", "Redux"]
+  
+  webTechs.reverse() // after sorting we can reverse it
+  console.log(webTechs) // ["Redux", "React", "Node", "MongoDB", "JavaScript", "HTML", "CSS"]
+
+  const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+  const backEnd = ['Node','Express', 'MongoDB']
+  const fullStack = [frontEnd, backEnd]
+  console.log(fullStack)   // [["HTML", "CSS", "JS", "React", "Redux"], ["Node", "Express", "MongoDB"]]
+  console.log(fullStack.length)  // 2
+  console.log(fullStack[0])  // ["HTML", "CSS", "JS", "React", "Redux"]
+  console.log(fullStack[1]) // ["Node", "Express", "MongoDB"]
