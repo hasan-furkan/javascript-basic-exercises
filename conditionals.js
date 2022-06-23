6f7bbd6a3ca3641ddd4f16fbb80f9f96b4e2e5a5
hasan-furkan <81443587+hasan-furkan@users.noreply.github.com>
2022-06-23 13:10:45 +0300

    conditionals

---
+++ conditionals.js
@@ -0,0 +1,62 @@
+let isRaining = true
+if (isRaining) {
+  console.log('You need a rain coat.')
+} else {
+  console.log('No need for a rain coat.')
+}
+// You need a rain coat.
+
+isRaining = false
+if (isRaining) {
+  console.log('You need a rain coat.')
+} else {
+  console.log('No need for a rain coat.')
+}
+// No need for a rain coat.
+
+// if else if else
+let weather = 'sunny'
+if (weather === 'rainy') {
+  console.log('You need a rain coat.')
+} else if (weather === 'cloudy') {
+  console.log('It might be cold, you need a jacket.')
+} else if (weather === 'sunny') {
+  console.log('Go out freely.')
+} else {
+  console.log('No need for rain coat.')
+}
+
+let dayUserInput = prompt('What day is today ?')
+let day = dayUserInput.toLowerCase()
+
+switch (day) {
+  case 'monday':
+    console.log('Today is Monday')
+    break
+  case 'tuesday':
+    console.log('Today is Tuesday')
+    break
+  case 'wednesday':
+    console.log('Today is Wednesday')
+    break
+  case 'thursday':
+    console.log('Today is Thursday')
+    break
+  case 'friday':
+    console.log('Today is Friday')
+    break
+  case 'saturday':
+    console.log('Today is Saturday')
+    break
+  case 'sunday':
+    console.log('Today is Sunday')
+    break
+  default:
+    console.log('It is not a week day.')
+}
+
+
+isRaining = true
+isRaining
+  ? console.log('You need a rain coat.')
+  : console.log('No need for a rain coat.')
